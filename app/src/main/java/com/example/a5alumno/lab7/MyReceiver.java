0c8d5d6835c27654509f89582b283eafd58493a9
Raul Garcia <luzontuells@gmail.com>
2016-11-23 17:33:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a5alumno/lab7/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.example.a5alumno.lab7;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e("Mensaje","AIRPLEIN MOUD");
+    }
+}
