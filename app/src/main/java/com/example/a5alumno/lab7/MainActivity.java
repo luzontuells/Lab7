dbfa09d71f342489a96713346e23595340e05a8a
Raul Garcia <luzontuells@gmail.com>
2016-11-24 09:44:37 +0100

    24/11 - 9:45

--- app/src/main/java/com/example/a5alumno/lab7/MainActivity.java
+++ app/src/main/java/com/example/a5alumno/lab7/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.a5alumno.lab7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater mMenuInflater = this.getMenuInflater();
+        mMenuInflater.inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        switch (item.getItemId()){
+            case R.id.addItem:
+                Log.e("Item","Add Pressed");
+                return true;
+                //break;
+            case R.id.likeItem:
+                Log.e("Item","Like Pressed");
+                return true;
+                //break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
