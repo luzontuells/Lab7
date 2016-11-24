d378db15358083324c9b290afbf94f78c2cb5983
Raul Garcia <luzontuells@gmail.com>
2016-11-24 12:26:05 +0100

    24/11 - 12:25

--- app/src/main/java/com/example/a5alumno/lab7/MainActivity.java
+++ app/src/main/java/com/example/a5alumno/lab7/MainActivity.java
@@ -2,17 +2,36 @@ package com.example.a5alumno.lab7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActionMode.Callback{
+
+
+    /*
+     *   La aplicación a implementar incluye dos elementos Activity con layouts muy simples, aunque diferentes. En
+     *   el primer caso la App Bar incluye la tradicional Action Bar, mientras que en el segundo alberga la novedosa
+     *   Toolbar, más flexible y robusta.
+     *   Además, en lo referente a menús contextuales, el primer layout incluye un clásico menú contextual flotante,
+     *   mientras que el segundo implementa lo que se conoce como Contextual Action Mode, y que convive con la
+     *   Toolbar.
+     *   Por último, a una de las opciones del segundo layout se le ha agregado un pop-up menú, mostrando así una
+     *   de las clásicas alternativas que hay en Android para presentar información adicional al usuario.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView launchMe = (ImageView) this.findViewById(R.id.launcher);
+        this.registerForContextMenu(launchMe);
     }
 
     @Override
@@ -20,6 +39,29 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mMenuInflater = this.getMenuInflater();
         mMenuInflater.inflate(R.menu.menu_principal, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater mContMenuInflater = this.getMenuInflater();
+        mContMenuInflater.inflate(R.menu.menu_principal, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addItem:
+                Log.e("Item", "Add Pressed");
+                return true;
+            //break;
+            case R.id.likeItem:
+                Log.e("Item", "Like Pressed");
+                return true;
+            //break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -38,5 +80,27 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        MenuInflater mMenuInflater = this.getMenuInflater();
+        mMenuInflater.inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+
     }
 }
