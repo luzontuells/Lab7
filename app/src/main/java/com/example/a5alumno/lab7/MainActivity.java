4d15b419e81a9e05b93b29c856e78278da931c97
Raul Garcia <luzontuells@gmail.com>
2016-11-24 13:48:08 +0100

    24/11 - 13:48

--- app/src/main/java/com/example/a5alumno/lab7/MainActivity.java
+++ app/src/main/java/com/example/a5alumno/lab7/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.a5alumno.lab7;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -11,7 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements ActionMode.Callback{
+public class MainActivity extends AppCompatActivity implements ActionMode.Callback, View.OnLongClickListener{
 
 
     /*
@@ -25,62 +29,25 @@ public class MainActivity extends AppCompatActivity implements ActionMode.Callba
      *   de las clásicas alternativas que hay en Android para presentar información adicional al usuario.
      */
 
+    ActionMode mActionMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ImageView launchMe = (ImageView) this.findViewById(R.id.launcher);
-        this.registerForContextMenu(launchMe);
+//        this.registerForContextMenu(launchMe);
+        launchMe.setOnLongClickListener(this);
+
+        Toolbar mChildToolbar = (Toolbar) this.findViewById(R.id.my_toolbar_second);
+        this.setSupportActionBar(mChildToolbar);
+        // Enable the Up button (A Back button, actually)
+        // Make sure the corresponding import relates to the suppport v7 library
+//        ActionBar mActionBar = this.getSupportActionBar();
+//        mActionBar.setDisplayHomeAsUpEnabled(true);   // This line actually enables the UP button
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater mMenuInflater = this.getMenuInflater();
-        mMenuInflater.inflate(R.menu.menu_principal, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater mContMenuInflater = this.getMenuInflater();
-        mContMenuInflater.inflate(R.menu.menu_principal, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.addItem:
-                Log.e("Item", "Add Pressed");
-                return true;
-            //break;
-            case R.id.likeItem:
-                Log.e("Item", "Like Pressed");
-                return true;
-            //break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-
-        switch (item.getItemId()){
-            case R.id.addItem:
-                Log.e("Item","Add Pressed");
-                return true;
-                //break;
-            case R.id.likeItem:
-                Log.e("Item","Like Pressed");
-                return true;
-                //break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     @Override
     public boolean onCreateActionMode(ActionMode mode, Menu menu) {
@@ -96,11 +63,36 @@ public class MainActivity extends AppCompatActivity implements ActionMode.Callba
 
     @Override
     public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+//        if(item.getItemId()==R.id.button_second){
+//            PopupMenu mPopMenu = new PopupMenu(this, this.findViewById(R.id.addItem));
+//            MenuInflater mMenuInflater = mPopMenu.getMenuInflater();
+//            mMenuInflater.inflate(R.menu.popup_menu, mPopMenu.getMenu());
+//            // The pop-up menu is configured to be clickable and later is shown
+//            mPopMenu.setOnMenuItemClickListener(this);
+//            mPopMenu.show();
+//        }
         return false;
     }
 
     @Override
     public void onDestroyActionMode(ActionMode mode) {
 
+    }
+
+
+    @Override
+    public boolean onLongClick(View view) {
+        if (view.getId() == R.id.button_second)
+        {
+            // if ActionMode is null "not started"
+            if (mActionMode != null) { return false; }
+            // Start the CAB
+            this.mActionMode = this.startActionMode(this);
+            view.setSelected(true);
+            return true;
+        }
+        else
+            return false;
     }
 }
--- app/src/main/java/com/example/a5alumno/lab7/SecondActivity.java
+++ app/src/main/java/com/example/a5alumno/lab7/SecondActivity.java
@@ -1,7 +1,16 @@
 package com.example.a5alumno.lab7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +18,69 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        ImageView launchMe = (ImageView) this.findViewById(R.id.button_second);
+        this.registerForContextMenu(launchMe);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        MenuInflater mMenuInflater = this.getMenuInflater();
+//        mMenuInflater.inflate(R.menu.menu_secundario, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item)
+//    {
+//
+//        switch (item.getItemId()){
+//            case R.id.addItem:
+//                Log.e("Item","Add Pressed, Second Activity");
+//                return true;
+//            //break;
+//            case R.id.likeItem:
+//                Log.e("Item","Like Pressed, Second Activity");
+//                return true;
+//            //break;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater mContMenuInflater = this.getMenuInflater();
+        mContMenuInflater.inflate(R.menu.menu_principal, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addItem:
+                Log.e("Item", "Start Pressed, First Activity");
+                Intent intent = new Intent(this, SecondActivity.class);
+                startActivity(intent);
+                return true;
+            //break;
+            case R.id.likeItem:
+                Log.e("Item", "Like Pressed, First Activity");
+                return true;
+            //break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+//    public void showPopup(View view){
+//        PopupMenu mPopMenu = new PopupMenu(this, this.findViewById(R.id.addItem));
+//        MenuInflater mMenuInflater = mPopMenu.getMenuInflater();
+//        mMenuInflater.inflate(R.menu.popup_menu, mPopMenu.getMenu());
+//// The pop-up menu is configured to be clickable and later is shown
+//        mPopMenu.setOnMenuItemClickListener(this);
+//        mPopMenu.show();
+//    }
+
+
 }
